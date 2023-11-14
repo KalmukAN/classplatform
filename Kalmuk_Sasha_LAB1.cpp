87cbcf077dd0fe098a8eed9cffe79925de59c62e
KalmukAN <144778782+KalmukAN@users.noreply.github.com>
2023-11-14 10:50:50 +0300

    Kalmuk_LAB_0
    Выводит "Hello world", а также запрос ввода символа

---
+++ Kalmuk_Sasha_LAB1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+ int main()
+ {
+    cout << "Hello, world!" << endl;
+    setlocale(LC_ALL, "Russian");
+    char Simvol;
+    cout << "Введите символ: ";
+    cin >> Simvol;
+    cout << "Вы ввели: " << Simvol << endl;
+
+}
+
